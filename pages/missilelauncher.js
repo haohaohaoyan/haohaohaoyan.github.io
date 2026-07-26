b755deb0853e738fc7f5922dff1ceb89b0ce311a
Haoyan Li <haoyanli234@gmail.com>
2026-07-26 16:48:03 -0400

    Missile launch button pulls from spritesheet instead now, can have many animations playing asynchronously without needing to redownload a bunch of times

--- pages/missilelauncher.js
+++ pages/missilelauncher.js
@@ -5,14 +5,26 @@ var assignedContext = assignedWindow.querySelector("iframe").contentWindow.docum
 var bigRedButton = assignedContext.querySelector("#bigredbutton");
 
 bigRedButton.addEventListener("click", () => {
-    var explosionImage = document.createElement("img");
+    // create explosion image and set things
+    var explosionImage = document.createElement("div");
+    explosionImage.style.width = "424px"; // rounded
+    explosionImage.style.height = "408px";
     explosionImage.style.position = "absolute";
     explosionImage.style.pointerEvents = "none";
-    explosionImage.src = `pages/assets/kaboom.gif?${Math.random()}`; // relative to index, randomized link forces loading multiple
-    explosionImage.style.top = `${Math.floor(Math.random() * window.innerHeight)}px`;
-    explosionImage.style.left = `${Math.floor(Math.random() * window.innerWidth)}px`;
+    explosionImage.style.zIndex = "999";
+    explosionImage.style.imageRendering = "pixelated";
+    explosionImage.style.backgroundImage = "url('pages/assets/kaboom-sprites.png')"; // relative to index
+    explosionImage.style.backgroundPositionX = "6840px";
+    explosionImage.style.backgroundSize = "6840px";
+    explosionImage.style.top = `${Math.floor(Math.random() * window.innerHeight - 408)}px`;
+    explosionImage.style.left = `${Math.floor(Math.random() * window.innerWidth - 424)}px`;
     document.body.insertAdjacentElement("beforeend", explosionImage);
-    setTimeout(() => {
-        explosionImage.remove();
-    }, 1360)
+    var explosionFrame = 0;
+    setInterval(() => {
+        if (explosionFrame == 16) {
+            explosionImage.remove();
+        };
+        explosionImage.style.backgroundPositionX = `${6840 - explosionFrame * 424}px`;
+        explosionFrame += 1;
+    }, 80)
 })
