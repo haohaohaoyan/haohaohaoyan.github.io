dc1fa5f00504cb0d47389270b8075892e48e7ece
Haoyan Li <haoyanli234@gmail.com>
2026-07-28 13:33:12 -0400

    Made a really basic three.js thing, the scripts are independent on each iframe but from what I can see it's cached and reused so no redownloads are necessary

--- os.js
+++ os.js
@@ -227,11 +227,10 @@ if (window.innerHeight >= window.innerWidth * 1.3) {
 };
 
 // converts vw to pixels because that's what the stretch functions expect
-createWindow("pages/start.html", false, `${window.innerWidth * 0.3}px`, `${window.innerHeight * 0.5 - window.innerWidth * 0.15}px`, `${window.innerWidth * 0.4}px`, `${window.innerWidth * 0.3}px`);
+//createWindow("pages/start.html", false, `${window.innerWidth * 0.3}px`, `${window.innerHeight * 0.5 - window.innerWidth * 0.15}px`, `${window.innerWidth * 0.4}px`, `${window.innerWidth * 0.3}px`);
 
 // debug
-//createWindow("pages/draw.html");
-//createWindow("pages/missilebutton.html");
+createWindow("pages/modelspinner.html")
 
 console.log("Did you expect anything here?");
 
